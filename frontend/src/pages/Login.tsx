9a034243ca3a1c5009f0b7e4efbb70613201d2eb
Cynthia <lazokmarios7@gmail.com>
2024-07-18 11:34:09 +0200

    feat: create auth pages

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,63 @@
+import { Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { setCookie } from "../utils/cookies";
+
+import { useAuth } from "../components/context/AuthContext";
+import { NavLink } from "react-router-dom";
+import { BASE_URL } from "../utils/config";
+
+
+export const Login = ()=> {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {login} = useAuth()
+
+    const handleLogin = async () =>{  
+       
+        const request = await fetch(`${BASE_URL}/user/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+          });
+        const response = await request.json()
+        if(response.id && response.token){
+          
+          setCookie('user', response, 7)
+          login()
+          window.location.replace('/')
+        }
+        
+    }
+
+  return (
+    <div className="h-screen text-white w-screen flex flex-col items-center gap-5 justify-center">
+      <Typography variant="h3" component="h3">
+        Please LogIn
+      </Typography>
+      <div className="flex flex-col gap-5" >
+        <TextField
+          name="email"
+          label="Email"
+          variant="standard"
+          autoComplete="new-password"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          variant="standard"
+          autoComplete="new-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={handleLogin}>LogIn</Button>
+        <NavLink to="/register">You not registered?</NavLink>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,71 @@
+import { Alert, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+
+import { NavLink} from "react-router-dom";
+import { useAuth } from "../components/context/AuthContext";
+import { BASE_URL } from "../utils/config";
+
+export const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName]= useState("")
+  const [regError, setRegError] = useState(false);
+  const { login } = useAuth();
+
+  const handleRegister = async () => {
+    const request = await fetch(`${BASE_URL}/user/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password, name }),
+    });
+    const response = await request.json();
+    if (response) {
+      setRegError(false);
+      login();
+      window.location.replace('/login')
+    } else {
+      return setRegError(true);
+    }
+  };
+
+  return (
+    <div className="h-screen w-screen flex flex-col items-center gap-5 justify-center">
+      <Typography variant="h3" component="h3">
+        Registration
+      </Typography>
+      <div className="flex flex-col gap-5">
+        <TextField
+          name="name"
+          label="Full Name"
+          variant="standard"
+          autoComplete="new-password"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          name="email"
+          label="Email"
+          variant="standard"
+          autoComplete="new-password"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          variant="standard"
+          autoComplete="new-password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button onClick={handleRegister}>Register</Button>
+        <NavLink to="/login">You have account?</NavLink>
+      </div>
+      {regError && <Alert severity="error">Something went wrong.</Alert>}
+    </div>
+  );
+};

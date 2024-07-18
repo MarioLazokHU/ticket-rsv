0105cbe43de1828e64a0456e9618c8a325b955a1
Cynthia <lazokmarios7@gmail.com>
2024-07-18 11:32:00 +0200

    chore: add tw colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        dark: '#313131',
+        medium: '#313131',
+        yellow: '#C69749',
+      }
+    },
   },
   plugins: [],
 }
